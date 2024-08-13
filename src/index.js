0e5c3ab5458862b7fe5666cf2ca07341aab2aaa6
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-08-13 14:34:23 +0200

    Setup index.js and index.css
    The index.js will contain the
    homeModule when the page
    first loads.
    
    Create a css file and import it
    in the index.js. Select the
    and save the references of
    these elements on respective
    constants. These buttons will
    be used to render their
    respective modules.
    
    Create a function that creates
    a div, gives it a class and a
    textContent and append it to the
    the #content div (container).
    
    Change the text color of
    the #content on the index.css to
    confirm if the css is being
    imported successfully.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("We are now live!");
+import "./index.css";
+
+const container = document.querySelector("#content");
+
+const homeButton = document.querySelector("#home-button");
+const menuButton = document.querySelector("#menu-button");
+const aboutButton = document.querySelector("#about-button");
+
+
+function createHomeModule() {
+    const homeContainer = document.createElement("div");
+    homeContainer.classList.add("home-container");
+    homeContainer.textContent = "Je suis chez moi.";
+
+    container.appendChild(homeContainer);
+}
+
+createHomeModule()
+
+
