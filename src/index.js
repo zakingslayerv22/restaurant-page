21ff94458b31d3b0ea4112e54582bc10547aeac5
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-08-14 14:08:08 +0200

    Create home module content
    Create the structure of the
    hero in such a way that a
    heroLeft div contains the
    heroText div and the heroButton.
    The heroText div contains the
    heroH1 and heroParagraph. The
    heroRight div will contain an
    image.
    
    Add the respective classes and
    text content to the respective
    elements. Use template literals
    for the heroParagraph text content
    so that it can span multiple
    lines for better readability
    on the text editor and
    still be treated as a single
    string.

--- src/index.js
+++ src/index.js
@@ -15,9 +15,38 @@ const aboutButton = document.querySelector("#about-button");
 function createHomeModule() {
     const homeContainer = document.createElement("div");
     homeContainer.classList.add("home-container");
-    homeContainer.textContent = "Je suis chez moi.";
+
+
+    const heroLeft = document.createElement("div");
+    heroLeft.classList.add("hero-left-container");
+
+    const heroText = document.createElement("div");
+    heroText.classList.add("hero-text-container");
+
+    const heroH1 = document.createElement("h1");
+    heroH1.classList.add("hero-h1");
+    heroH1.textContent = "Feednotize Me";
+
+    const heroParagraph = document.createElement("p");
+    heroParagraph.classList.add("hero-paragraph");  
+    heroParagraph.textContent = `Since 1970, Feednotize Me has
+    been concerned with conjuring meals that has always left
+    even the finest of connoisseurs salivating with wonder."`
+
+    const heroButton = document.createElement("button");
+    heroButton.classList.add("hero-button");
+    heroButton.textContent = "Browse Menu >>"
+
+    const heroRight = document.createElement("div");
+    heroRight.classList.add("hero-image-container");
+    heroRight.textContent = `I am waiting for my image`;
+
+
 
     container.appendChild(homeContainer);
+    homeContainer.append(heroLeft, heroRight);
+    heroLeft.append(heroText, heroButton);
+    heroText.append(heroH1, heroParagraph);
 }
 
 createHomeModule()
