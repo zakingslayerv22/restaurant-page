f7fffeb853272ce295a06ef338772d5ee0b193d9
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-08-19 12:56:37 +0200

    Populate menu module
    Create a menu h3, that would
    serve as a title. Create a
    menu items container that would
    contain the menu items. Create
    six menu items (delicacies)
    
    Append the menu h3 and
    menu items container to the
    menu container. Append the
    menu items to the menu items
    container.

--- src/menu/menu.module.js
+++ src/menu/menu.module.js
@@ -5,8 +5,34 @@ export function createMenuModule () {
 
     const menuContainer = document.createElement("div");
     menuContainer.classList.add("menu-container");
-    menuContainer.textContent = "Puis-je voir votre menu ?";
+
+
+    const menuH3 = document.createElement("h3");
+    menuH3.classList.add("menu-h3");
+    menuH3.textContent = "Explore our array of delicacies."
+
+    const menuItemsContainer = document.createElement("div");
+    menuItemsContainer.classList.add("menu-items-container");
+
+    const caesarSalad = document.createElement("div");
+
+    const tomatoPasta = document.createElement("div");
+
+    const marinatedChicken = document.createElement("div");
+
+    const bakedPotatoes = document.createElement("div");
+
+    const saladeNicoise = document.createElement("div");
+
+    const bouillabaisse = document.createElement("div");
+
+
 
     container.appendChild(menuContainer);
+
+    menuContainer.append(menuH3, menuItemsContainer);
+
+    menuItemsContainer.append(caesarSalad, tomatoPasta, marinatedChicken, 
+        bakedPotatoes, saladeNicoise, bouillabaisse);
 }
 
