779a13c8231245d4283326e6e26921f177964365
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-08-19 15:27:26 +0200

    Add and import the menu images
    Create a menu-assets folder
    and an images folder inside it.
    Add the image files in the images
    folder and import them in the
    menu.module.js file.
    
    Create new
    HTMLImageElement objects with
    respect to each div in the menu
    items container and set
    its src attributes to the
    respective imported image.
    
    Append these new HTMLImageElement
    object (images) to their respective
    div elements.

--- src/menu/menu.module.js
+++ src/menu/menu.module.js
@@ -1,4 +1,10 @@
 import './menu.css'
+import CaesarSalad from './menu-assets/images/caesar-salad.webp';
+import TomatoPasta from './menu-assets/images/spaghetti-al-pomodoro.webp';
+import MarinatedChicken from './menu-assets/images/chicken-inasal.webp';
+import BakedPotatoes from './menu-assets/images/kumpir.webp';
+import SaladeNicoise from './menu-assets/images/salade-nicoise.webp';
+import Bouillabaisse from './menu-assets/images/bouillabaisse.webp';
 
 export function createMenuModule () {
     const container = document.querySelector("#content")
@@ -15,16 +21,29 @@ export function createMenuModule () {
     menuItemsContainer.classList.add("menu-items-container");
 
     const caesarSalad = document.createElement("div");
+    const caesarSaladImage = new Image();
+    caesarSaladImage.src = CaesarSalad;
 
     const tomatoPasta = document.createElement("div");
+    const tomatoPastaImage = new Image();
+    tomatoPastaImage.src = TomatoPasta;
 
     const marinatedChicken = document.createElement("div");
+    const marinatedChickenImage = new Image();
+    marinatedChickenImage.src = MarinatedChicken;
 
     const bakedPotatoes = document.createElement("div");
+    const bakedPotatoesImage = new Image();
+    bakedPotatoesImage.src = BakedPotatoes;
 
     const saladeNicoise = document.createElement("div");
+    const saladeNicoiseImage = new Image();
+    saladeNicoiseImage.src = SaladeNicoise;
+
 
     const bouillabaisse = document.createElement("div");
+    const bouillabaisseImage = new Image();
+    bouillabaisseImage.src = Bouillabaisse;
 
 
 
@@ -34,5 +53,12 @@ export function createMenuModule () {
 
     menuItemsContainer.append(caesarSalad, tomatoPasta, marinatedChicken, 
         bakedPotatoes, saladeNicoise, bouillabaisse);
+
+    caesarSalad.appendChild(caesarSaladImage);
+    tomatoPasta.appendChild(tomatoPastaImage);
+    marinatedChicken.appendChild(marinatedChickenImage);
+    bakedPotatoes.appendChild(bakedPotatoesImage);
+    saladeNicoise.appendChild(saladeNicoiseImage);
+    bouillabaisse.appendChild(bouillabaisseImage);
 }
 
